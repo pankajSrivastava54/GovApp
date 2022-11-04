66456c66d846f5803fb04f30597b4ec449877b40
Pankaj Srivastava <pankaj.srivastava@ey.com>
2022-11-03 20:50:21 -0700

    pie color

--- src/Tourism.js
+++ src/Tourism.js
@@ -253,15 +253,32 @@ const fetchTop15CountriesFTAs = (callback) => {
 
       // ],
       const chartData = {
-        
+        labels: newArr
+        .map((record) => record.country),
         datasets: [
           {
-            labels: newArr
-            .map((record) => record.country),
+
             data: newArr
               .map((record) => record.ftas_in_india_in_2019),
-            backgroundColor: "rgba(155, 99, 132, 0.6)",
-            borderWidth: 4,
+            //backgroundColor: "rgba(155, 99, 132, 0.6)",
+            //borderWidth: 4,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
           },
           // {
           //   labels: newArr
